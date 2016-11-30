96c33986d9d8478bfb7fda8efff197f0c618669c
jbouffard <jbouffard@azavea.com>
2016-11-30 18:20:50 -0500

    StreamingSegmentBytes can now read in segments both via arrays and lazily

--- raster/src/main/scala/geotrellis/raster/io/geotiff/StreamingSegmentBytes.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/StreamingSegmentBytes.scala
@@ -3,17 +3,16 @@ package geotrellis.raster.io.geotiff
 import geotrellis.util._
 import geotrellis.raster._
 import geotrellis.raster.io.geotiff.tags._
+import geotrellis.raster.io.geotiff.util._
 import geotrellis.vector.Extent
 
 import spire.syntax.cfor._
 import monocle.syntax.apply._
 
-import scala.math.Ordering.Implicits._
-
 class StreamingSegmentBytes(byteReader: StreamingByteReader,
 	segmentLayout: GeoTiffSegmentLayout,
 	extent: Extent,
-	tiffTags: TiffTags) {
+	tiffTags: TiffTags) extends SegmentBytes {
 
 	val gridBounds: GridBounds = {
 		val rasterExtent: RasterExtent =
@@ -26,9 +25,12 @@ class StreamingSegmentBytes(byteReader: StreamingByteReader,
 	private val colMax: Int = gridBounds.colMax
 	private val rowMax: Int = gridBounds.rowMax
 
-	val segments: Array[Int] = {
-		(0 until tiffTags.segmentCount).filter(x => {
+	val segments: Array[Int] = 0 until tiffTags.segmentCount toArray
+
+	val intersectingSegments: Array[Int] =
+		segments.filter(x => {
 			val segmentTransform = segmentLayout.getSegmentTransform(x)
+
 			val startCol: Int = segmentTransform.indexToCol(0)
 			val startRow: Int = segmentTransform.indexToRow(0)
 			val endCol: Int = startCol + segmentTransform.segmentCols
@@ -40,5 +42,49 @@ class StreamingSegmentBytes(byteReader: StreamingByteReader,
 			(startResult && endResult)
 
 		}).toArray
+
+	val remainingSegments: Array[Int] =
+		segments.filter(x => !intersectingSegments.contains(x))
+
+	val (offsets, byteCounts) =
+		if (tiffTags.hasStripStorage) {
+			val stripOffsets = (tiffTags &|->
+				TiffTags._basicTags ^|->
+				BasicTags._stripOffsets get)
+			
+			val stripByteCounts = (tiffTags &|->
+				TiffTags._basicTags ^|->
+				BasicTags._stripByteCounts get)
+
+			(stripOffsets.get, stripByteCounts.get)
+
+		} else {
+			val tileOffsets = (tiffTags &|->
+				TiffTags._tileTags ^|->
+				TileTags._tileOffsets get)
+
+			val tileByteCounts = (tiffTags &|->
+				TiffTags._tileTags ^|->
+				TileTags._tileByteCounts get)
+
+			(tileOffsets.get, tileByteCounts.get)
+		}
+	
+	private val compressedBytes: Array[Array[Byte]] = {
+		val result = Array.ofDim[Array[Byte]](offsets.size)
+				
+		cfor(0)(_ < intersectingSegments.size, _ + 1) { i =>
+			val value = intersectingSegments(i)
+			result(value) = byteReader.getSignedByteArray(byteCounts(value), offsets(value))
+		}
+		result
 	}
+
+	override val size = offsets.size
+
+	def getSegment(i: Int): Array[Byte] =
+		if (intersectingSegments.contains(i))
+			compressedBytes(i)
+		else
+			byteReader.getSignedByteArray(byteCounts(i), offsets(i))
 }
